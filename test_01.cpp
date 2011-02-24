6881f7c2c5007d7468db74b4614f07bbe15f9695
Tommy Ogden <t@tommyogden.com>
2011-02-24 13:28:17 +0100

    Created .gitignore file and a test file

---
+++ test_01.cpp
@@ -0,0 +1,16 @@
+#include "tommy.h"
+
+using namespace std;
+
+int main() {
+
+    tic();
+
+    for (int i = 0; i < 100000; i++)
+    cout << "Hello, world!" << lb;
+
+    toc();
+    
+    return 0;
+
+}
